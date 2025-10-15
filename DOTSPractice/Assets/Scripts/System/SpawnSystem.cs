130dd3af11974b185243a0697c6f2e78c49a45d6
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-10-16 06:26:43 +0900

    [add]ECB追加

--- DOTSPractice/Assets/Scripts/System/SpawnSystem.cs
+++ DOTSPractice/Assets/Scripts/System/SpawnSystem.cs
@@ -7,6 +7,12 @@ public partial class SpawnSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(World.Unmanaged).AsParallelWriter();
+        
+        new SpawnJob()
+        {
+            ECB = ecb
+        }.ScheduleParallel();
     }
 }
