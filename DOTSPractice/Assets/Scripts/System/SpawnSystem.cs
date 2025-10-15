9b67e7179a4e5440d8e5a379f530b7a6959b3034
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-10-16 06:05:23 +0900

    [add]システム追加

---
+++ DOTSPractice/Assets/Scripts/System/SpawnSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Entities;
+
+public partial class SpawnSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+
+    }
+}
